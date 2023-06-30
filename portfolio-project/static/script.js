9f20c1b8a56e5a71b509c9787b24bbecf2c928a7
jdutchfoy <jdutchfoy@gmail.com>
2023-06-30 14:44:30 -0700

    added additonal files some code

---
+++ portfolio-project/static/script.js
@@ -0,0 +1,10 @@
+// JavaScript code
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    // Code for scrolling down
+  } else {
+    // Code for scrolling up
+  }
+}
